35b3ea05a92a9513b5382ceff6ff1a60c829d622
Killian Landrieu <killian.landrieu@gmail.com>
2021-01-12 14:58:31 +0100

    Send message début

--- www/js/myscripts.js
+++ www/js/myscripts.js
@@ -1,5 +1,4 @@
-
-  function main() {
+function main() {
     var pions = document.querySelectorAll("#Jeu button");
     var joueurs = ["X", "O"];
     var tour = 0;
@@ -10,3 +9,18 @@
         joueurs[tour] +
         " c'est votre tour."
     );
+    for (var i = 0, len = pions.length; i < len; i++) {
+      pions[i].addEventListener("click", function() {
+        if (jeuEstFini) return;
+  
+        if (!estValide(this)) {
+          tour++;
+          tour = tour % 2;
+          afficheur.sendMessage("Joueur " + joueurs[tour] + " c'est à vous !");
+        }
+      });
+    }
+  }
+  
+  main();
+  
